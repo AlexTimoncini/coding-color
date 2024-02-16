621c342e475db5acb2654904e55083b87fbfe0cd
Alex Timoncini <alex@overcover.com>
2024-02-16 15:02:03 +0100

    fixing loader

--- js/handle-scroll.js
+++ js/handle-scroll.js
@@ -11,17 +11,18 @@ function preventDefaultForScrollKeys(e) {
     }
 }
 
-var supportsPassive = false;
+let supportsPassive = false;
 try {
     window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
         get: function () { supportsPassive = true; } 
     }));
 } catch(e) {}
 
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+let wheelOpt = supportsPassive ? { passive: false } : false;
+let wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
 function disableScroll() {
+    window.scrollTo(0, 0);
     window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
     window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
     window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
