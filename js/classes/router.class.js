900ef2a51d445c02890158b8dc26bf86c89f2d08
AlexTimoncini <alextimonci@gmail.com>
2024-02-21 23:16:18 +0100

    restored old router (github pages do not support subpages)

--- js/classes/router.class.js
+++ js/classes/router.class.js
@@ -6,19 +6,15 @@ export class Router {
     get(route, callback) {
         this.routes[this.root + route] = callback;
     }
+    //OLD ROUTER
     start() {
-        window.addEventListener('popstate', this.handleURLChange.bind(this));
-        window.addEventListener('load', this.handleURLChange.bind(this));
-
-        if (window.location.hash) {
-            const currentURL = window.location.href.split('#')[0];
-            history.replaceState({}, document.title, currentURL);
-        }
+        window.addEventListener('hashchange', this.handleHashChange.bind(this));
+        window.addEventListener('load', this.handleHashChange.bind(this));
     }
 
-    handleURLChange() {
-        const currentPath = window.location.pathname;
-        const currentRoute = this.root + (currentPath || '/');
+    handleHashChange() {
+        const currentHash = window.location.hash.substring(1);
+        const currentRoute = this.root + (currentHash || '/');
 
         if (currentRoute in this.routes) {
             this.routes[currentRoute]();
@@ -31,9 +27,35 @@ export class Router {
         }
     }
 
-    navigateTo(route) {
-        const newURL = this.root + route;
-        history.pushState({}, document.title, newURL);
-        this.handleURLChange();
-    }
+    //NEW ROUTER
+    // start() {
+    //     window.addEventListener('popstate', this.handleURLChange.bind(this));
+    //     window.addEventListener('load', this.handleURLChange.bind(this));
+
+    //     if (window.location.hash) {
+    //         const currentURL = window.location.href.split('#')[0];
+    //         history.replaceState({}, document.title, currentURL);
+    //     }
+    // }
+
+    // handleURLChange() {
+    //     const currentPath = window.location.pathname;
+    //     const currentRoute = this.root + (currentPath || '/');
+
+    //     if (currentRoute in this.routes) {
+    //         this.routes[currentRoute]();
+    //     } else {
+    //         console.log('Route not found, loading default route');
+    //         const defaultRoute = this.root + '/';
+    //         if (defaultRoute in this.routes) {
+    //             this.routes[defaultRoute]();
+    //         }
+    //     }
+    // }
+
+    // navigateTo(route) {
+    //     const newURL = this.root + route;
+    //     history.pushState({}, document.title, newURL);
+    //     this.handleURLChange();
+    // }
 }
