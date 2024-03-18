ffd8f0f95f760e4fe3cb03aca573a51e0c3b763c
AlexTimoncini <alextimonci@gmail.com>
2024-03-18 11:34:01 +0100

    deleted old css file + data-default to delete old script files not default

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 //ROUTER
 import { Router } from './js/classes/router.class.js'
 //let router = new Router('http://127.0.0.1:8000');
-//let router = new Router('http://localhost:8000');
-let router = new Router('https://coding-color.it');
+let router = new Router('http://localhost:8000');
+//let router = new Router('https://coding-color.it');
 //rotte
 router.get('/', function(){
     buildPage('home.html',
@@ -85,6 +85,8 @@ async function buildPage(mainHTML, css, src){
         document.getElementById("app").insertAdjacentHTML("afterend", html);
     }
     async function script() {
+        const existingScripts = document.querySelectorAll('script:not([data-default=true])');
+        existingScripts.forEach(spt => spt.remove());
         if (src) {
             for (const url of src) {
                 await loadScript(url);
