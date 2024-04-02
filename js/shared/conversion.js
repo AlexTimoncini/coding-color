2afd506ba1f4b9ca1acad72b4e62bf5b8f9d47a0
Alex Timoncini <alex@overcover.com>
2024-04-02 17:05:22 +0200

    you can now deselect to format radio btns

--- js/shared/conversion.js
+++ js/shared/conversion.js
@@ -3,6 +3,23 @@ document.getElementById('ab_op').addEventListener('change', ()=>{
     document.querySelectorAll("#opacity .parameter:not(:first-of-type)").forEach(el=> el.classList.toggle("no-events"))
 })
 
+//as now we can select no final format (so it won't be changed) we have to make those radios button unselectable
+//toggle no-events on opacity
+document.querySelectorAll('#to input[type="radio"]').forEach(btn => {
+    let id = btn.id;
+    document.querySelector('label[for="' + id + '"]').addEventListener('click', (e) => {
+        if (btn.checked) {
+            e.preventDefault()
+            btn.checked = false
+        }
+    })
+})
+
+
+document.getElementById('ab_op').addEventListener('change', ()=>{
+    document.querySelectorAll("#opacity .parameter:not(:first-of-type)").forEach(el=> el.classList.toggle("no-events"))
+})
+
 //validate opaciti value
 document.getElementById('op').addEventListener('input', (e)=>{
     const inputElement = e.target;
